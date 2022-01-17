ce9934e7a925254eee63a5ab0db6401f93fe1435
BerryOMG <mogu_house@hotmail.com>
2022-01-17 13:40:28 -0500

    postgres configuration

--- jyjewellery/settings.py
+++ jyjewellery/settings.py
@@ -89,13 +89,26 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import os
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'defalut':{
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : os.environ['RDS_DB_NAME'],
+        'USER' : os.environ['RDS_USERNAME'],
+        'PASSWORD' : os.environ['RDS_PASSWORD'],
+        'HOST' : os.environ['RDS_HOSTNAME'],
+        'PORT' : os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
